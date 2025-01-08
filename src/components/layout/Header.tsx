e60d9995d8ed9c6bfb7a945854f0ff4f645f43ad
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-01-08 15:15:16 -0800

    Add tailwind styles, add header prototype

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,24 @@
+export default function Header() {
+  return (
+    <div className="p-3 bg-cyan-100">
+      <nav className="justify-center flex flex-row space-x-5 mx-3">
+        <h1 className="font-bold text-xl">Logo</h1>
+        <a href="#" className="hidden md:block text-lg font-semibold">
+          Hero
+        </a>
+        <a href="#" className="hidden md:block text-lg font-semibold">
+          Intro
+        </a>
+        <a href="#" className="hidden md:block text-lg font-semibold">
+          Projects
+        </a>
+        <a href="#" className="hidden md:block text-lg font-semibold">
+          Skills
+        </a>
+        <a href="#" className="hidden md:block text-lg font-semibold">
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
+}
