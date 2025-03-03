f1a56390778f70033397eb893d90875866afead9
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-03-03 11:23:44 -0800

    Update carousel arrows

--- src/components/sections/Skills/Carousel.tsx
+++ src/components/sections/Skills/Carousel.tsx
@@ -2,82 +2,85 @@
 import { useEffect, useState } from "react";
 
 interface Props {
-  images: Array<string>;
+    images: Array<string>;
 }
 
 export default function Carousel({ images }: Props) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [autoScroll, setAutoScroll] = useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [autoScroll, setAutoScroll] = useState(1);
 
-  function handleNext() {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  }
-
-  function handlePrev() {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  }
-
-  useEffect(() => {
-    if (autoScroll) {
-      // use setTimeout here because we want the user's input to reset the timer
-      const timedUpdate = setTimeout(
-        () => setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length),
-        5000
-      );
-
-      return () => {
-        clearTimeout(timedUpdate);
-      };
+    function handleNext() {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }
-  }, [images.length, currentIndex, autoScroll]);
 
-  return (
-    <div
-      className="relative w-full h-42 bg-orange-700"
-      onMouseEnter={() => setAutoScroll(0)}
-      onMouseLeave={() => setAutoScroll(1)}
-    >
-      <div className="flex flex-row overflow-hidden bg-indigo-400">
-        {images.map((item) => (
-          <img
-            key={item}
-            className={`duration-300 transition-transform ease-in-out object-cover w-full h-full -translate-x-[${
-              100 * currentIndex
-            }%]`}
-            src={item}
-            alt=""
-          />
-        ))}
-      </div>
-      {/* arrow buttons only appear if there's more than one image*/}
-      {images.length > 1 && (
-        <>
-          <button
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-2xl"
-            onClick={handlePrev}
-          >
-            <div
-              className="flex justify-center items-center w-9 h-9 rounded-full bg-slate-400 
-        opacity-80 transition ease-in-out hover:bg-slate-500 hover:scale-110 active:bg-slate-700 font-mono"
-            >
-              <p>{"<"}</p>
+    function handlePrev() {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    }
+
+    useEffect(() => {
+        if (autoScroll) {
+            // use setTimeout here because we want the user's input to reset the timer
+            const timedUpdate = setTimeout(
+                () =>
+                    setCurrentIndex(
+                        (prevIndex) => (prevIndex + 1) % images.length
+                    ),
+                5000
+            );
+
+            return () => {
+                clearTimeout(timedUpdate);
+            };
+        }
+    }, [images.length, currentIndex, autoScroll]);
+
+    return (
+        <div
+            className="relative w-full h-42 bg-orange-700"
+            onMouseEnter={() => setAutoScroll(0)}
+            onMouseLeave={() => setAutoScroll(1)}
+        >
+            <div className="flex flex-row overflow-hidden bg-indigo-400">
+                {images.map((item) => (
+                    <img
+                        key={item}
+                        className={`duration-300 transition-transform ease-in-out object-cover w-full h-full -translate-x-[${
+                            100 * currentIndex
+                        }%]`}
+                        src={item}
+                        alt=""
+                    />
+                ))}
             </div>
-          </button>
-          <button
-            className="absolute right-3 top-1/2 -translate-y-1/2 text-2xl"
-            onClick={handleNext}
-          >
-            <div
-              className="flex justify-center items-center w-9 h-9 rounded-full bg-slate-400 
-        opacity-80 transition ease-in-out hover:bg-slate-500 hover:scale-110 active:bg-slate-700 font-mono"
-            >
-              <p>{">"}</p>
-            </div>
-          </button>
-        </>
-      )}
-    </div>
-  );
+            {/* arrow buttons only appear if there's more than one image*/}
+            {images.length > 1 && (
+                <>
+                    <button
+                        className="absolute left-3 top-1/2 -translate-y-1/2 text-2xl"
+                        onClick={handlePrev}
+                    >
+                        <div
+                            className="flex justify-center items-center w-9 h-9 rounded-full text-white 
+        opacity-80 transition ease-in-out hover:bg-slate-500 hover:scale-110 active:bg-slate-700 cursor-pointer font-mono"
+                        >
+                            <p>{"<"}</p>
+                        </div>
+                    </button>
+                    <button
+                        className="absolute right-3 top-1/2 -translate-y-1/2 text-2xl"
+                        onClick={handleNext}
+                    >
+                        <div
+                            className="flex justify-center items-center w-9 h-9 rounded-full text-white 
+        opacity-80 transition ease-in-out hover:bg-slate-500 hover:scale-110 active:bg-slate-700 cursor-pointer font-mono"
+                        >
+                            <p>{">"}</p>
+                        </div>
+                    </button>
+                </>
+            )}
+        </div>
+    );
 }
--- src/components/sections/Skills/MyDialog.tsx
+++ src/components/sections/Skills/MyDialog.tsx
@@ -2,48 +2,48 @@ import { ReactNode } from "react";
 import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 
 interface Props {
-  children?: ReactNode;
-  open: boolean;
-  setOpen: (newValue: boolean | ((prevValue: boolean) => boolean)) => void;
+    children?: ReactNode;
+    open: boolean;
+    setOpen: (newValue: boolean | ((prevValue: boolean) => boolean)) => void;
 }
 
 export default function MyDialog({ children, open, setOpen }: Props) {
-  return (
-    <Dialog
-      open={open}
-      onClose={setOpen}
-      className="relative z-10 overflow-scroll"
-    >
-      <DialogBackdrop
-        transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-leave:duration-200 data-enter:ease-out data-leave:ease-in"
-      />
-      <div className="fixed inset-0 z-10 w-screen">
-        <div className="flex min-h-full justify-center items-center p-4 text-center sm:p-0">
-          <DialogPanel
-            transition
-            className="relative transform rounded-xl 
+    return (
+        <Dialog
+            open={open}
+            onClose={setOpen}
+            className="relative z-10 overflow-scroll"
+        >
+            <DialogBackdrop
+                transition
+                className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-leave:duration-200 data-enter:ease-out data-leave:ease-in"
+            />
+            <div className="fixed inset-0 z-10 w-screen">
+                <div className="flex min-h-full justify-center items-center p-4 text-center sm:p-0">
+                    <DialogPanel
+                        transition
+                        className="relative transform rounded-xl 
               bg-white text-left shadow-xl transition-all data-closed:translate-y-4 
               data-closed:opacity-0 data-enter:duration-300 data-leave:duration-200 
               data-enter:ease-out data-leave:ease-in sm:my-8 w-full max-w-lg 
               sm:data-closed:translate-y-0 sm:data-closed:scale-95"
-          >
-            <div className=" px-4 pb-4 pt-5 sm:p-6 sm:pb-4 rounded-xl">
-              <button
-                className="transition ease-in-out cursor-pointer bg-slate-200 py-1 px-6 mb-3 right-0 
+                    >
+                        <div className=" px-4 pb-4 pt-5 sm:p-6 sm:pb-4 rounded-xl">
+                            <button
+                                className="transition ease-in-out cursor-pointer bg-slate-200 py-1 px-6 mb-3 right-0 
         rounded-sm font-black drop-shadow-xs hover:bg-slate-300 hover:scale-110
         active:bg-slate-500"
-                onClick={() => setOpen(false)}
-              >
-                X
-              </button>
-              <div className="sm:flex sm:items-start overflow-y-auto max-h-[70vh]">
-                {children}
-              </div>
+                                onClick={() => setOpen(false)}
+                            >
+                                X
+                            </button>
+                            <div className="sm:flex sm:items-start overflow-y-auto max-h-[70vh]">
+                                {children}
+                            </div>
+                        </div>
+                    </DialogPanel>
+                </div>
             </div>
-          </DialogPanel>
-        </div>
-      </div>
-    </Dialog>
-  );
+        </Dialog>
+    );
 }
